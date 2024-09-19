12d5e963a41a714f9013ef172625f50f893a79b3
dmaybao <dmaybao@teleco.upv.es>
2024-09-19 17:50:26 +0200

    Reinicio

---
+++ Práctica 1/v1.0/Fifo/lleno.v
@@ -0,0 +1,10 @@
+module lleno(F_EMPTY_N, F_FULL_N, WRITE, READ, DATA_IN, DATA_OUT, COUNT, clk);
+
+input READ, WRITE, RESET_N, clk;
+input [7:0] DATA_IN;
+
+output F_FULL_N, F_EMPTY_N;
+output [7:0] DATA_OUT;
+
+endmodule 
+

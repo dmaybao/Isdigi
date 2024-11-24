3a2a3e9ac32fbc83188c6bd0355476847ff4aa40
Daniel Mayoral <159618399+dmaybao@users.noreply.github.com>
2024-11-24 21:50:23 +0100

    Quartus

---
+++ v1.6Q/Divisor_Segmentado.sv
@@ -0,0 +1,78 @@
+//-----------------------------------------------------------------------------
+// Universitat Politècnica de València   |  2024-2025
+// Isdigi           3º Estit
+// 
+// Date:            22/11/2024
+// Names:           Daniel Mayoral Baños
+// Module Name:     Divisor_Segmentado
+// Project Name:    Divisor_Segmentado
+// Description:     Módulo de diseño para Divisor_Segmentado
+//                 
+//
+// Dependencies:    .CLK(CLK), .RSTa(RSTa), .Start(Start), .Num(Num), 
+//		            .Den(Den), .Coc(Coc), .Res(Res), .Done(Done)
+// Parameters: 	    tamanyo = 32
+//
+// Revision: v 1.5
+// Additional Comments:
+//-----------------------------------------------------------------------------
+
+module Divisor_Segmentado
+#(parameter tamanyo=32) 
+(input logic CLK, RSTa,Start,
+input logic[tamanyo-1:0]Num,Den,
+output logic [tamanyo-1:0]Coc,Res,
+output logic Done
+);
+
+//Nueva forma de contador
+localparam etapas=2*tamanyo+1;
+
+logic [etapas-1:0][tamanyo-1:0]ACCU,Q,M;
+logic [etapas-1:0]SignNum,SignDen,Start_aux;
+
+
+always_ff @(posedge CLK or negedge RSTa)
+if(!RSTa)
+	begin
+		{ACCU,Q,M,SignNum,SignDen,Start_aux}<='0;		
+	end
+else begin
+	      Start_aux[etapas-1]<=Start;
+	
+			ACCU[etapas-1]<='0;
+			Start_aux[etapas-1]<=Start;
+			SignNum[etapas-1]<=Num[tamanyo-1];
+			SignDen[etapas-1]<=Den[tamanyo-1];
+			Q[etapas-1]<=Num[tamanyo-1]?(~Num+1):Num;
+			M[etapas-1]<=Den[tamanyo-1]?(~Den+1):Den;
+		
+		
+	for(int i=(etapas-2);i>=0;i=i-1)
+		begin
+			Start_aux[i]<=Start_aux[i+1];
+			M[i] <= M[i+1];
+			SignNum[i]<=SignNum[i+1];
+			SignDen[i]<=SignDen[i+1];
+			if(i%2==1)
+			
+				{ACCU[i],Q[i]}<={ACCU[1+i][tamanyo-2:0],Q[1+i],1'b0};
+			else
+				if(ACCU[i+1]>=M[i+1])
+				begin
+					Q[i]<=Q[i+1]+1;
+					ACCU[i]<=ACCU[i+1]-M[i+1];
+				end
+				else 
+				begin
+					Q[i] <= Q[i+1];
+					ACCU[i]<=ACCU[i+1];
+				end
+			end
+			
+		end
+		
+assign Done =Start_aux[0];
+assign Coc = (SignNum[0]^SignDen[0])? (~Q[0]+1):Q[0];
+assign Res = SignNum[0]? (~ACCU[0]+1):ACCU[0];
+endmodule
